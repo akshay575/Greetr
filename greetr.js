5d850dddbfcb4dc6dbdcca4472cdbfe003615252
Akshay Arora <akshay575@yahoo.com>
2019-04-09 13:13:18 +0530

    add methods to greetr library

--- greetr.js
+++ greetr.js
@@ -4,7 +4,80 @@
         return new Greetr.init(firstname, lastname, language);
     }
 
-    Greetr.prototype = {};
+    var supportedLangs = ['en', 'es', 'in'];
+
+    var greeting = {
+        en: 'Hello', 
+        es: 'Hola', 
+        in: 'Namaste'
+    };
+
+    var formalGreeting = {
+        en: 'Greeting',
+        es: 'Saludos',
+        in: 'Namaste'
+    }
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion',
+        in: 'Upbhokta'
+    }
+
+    Greetr.prototype = {
+
+        fullName: function() {
+            return this.firstname + ' ' + this.lastname;
+        },
+
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Error: Language not supported!";
+            }
+        },
+
+        greeting: function() {
+            return greeting[this.language] + ' ' + this.firstname;
+        },
+
+        formalGreeting: function() {
+            return formalGreeting[this.language] + ' ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            if(formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if(console) {
+                console.log(msg);
+            }
+
+            return this;
+        },
+
+        log: function() {
+            if(console) {
+                console.log(logMessages[this.language] + ' ' + this.fullName());
+            }
+
+            return this;
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+
+    };
 
     Greetr.init = function(firstname, lastname, language) {
         var self = this;
